f4edb0ca8e5819a3b38617cd5c51dcaa27ef16e2
fatihkirpi <extfatihk@d-teknoloji.com.tr>
2021-06-24 21:06:28 +0300

    firstcommit

---
+++ first.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+var fist = "my code"
+let a1:Int = 1
+print(a1)
+String(a1)
+
+let compare: (Int, Int)->Int={(x,y)in
+return x+y
+}
+compare(3,5)
+exercism.io
